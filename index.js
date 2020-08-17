dd2999be367455b50c64641191a99f2f54cdb165
nickgarfield <ntgarfield@gmail.com>
2020-08-17 13:30:39 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4: uuidv4 } = require("uuid");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`); // Redirect to a new room
+});
+
+app.get("/:id", (req, res) => {
+  res.render("room", { roomId: req.params.id });
+});
+
+io.on("connection", socket => {
+  socket.on("join-room", (roomId, peerId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("peer-joined-room", peerId);
+
+    socket.on("disconnect", () =>
+      socket.to(roomId).broadcast.emit("peer-exited-room", peerId)
+    );
+  });
+});
+
+server.listen(3000);
---
+++ public/room.js
@@ -0,0 +1,40 @@
+const socket = io("/");
+const peerGrid = document.getElementById("peer-grid");
+const myPeer = new Peer({ host: "localhost", port: "3001" });
+const peers = {};
+
+myPeer.on("open", myPeerId => {
+  navigator.mediaDevices
+    .getUserMedia({ video: false, audio: true })
+    .then(myStream => {
+      myPeer.on("call", call => {
+        call.answer(myStream);
+        call.on("stream", peerStream => addPeer(call, peerStream));
+      });
+      socket.on("peer-joined-room", peerId => call(peerId, myStream));
+      socket.on("peer-exited-room", peerId => hangUp(peerId));
+      socket.emit("join-room", ROOM_ID, myPeerId);
+    });
+});
+
+function call(peerId, myStream) {
+  const call = myPeer.call(peerId, myStream);
+  call.on("stream", peerStream => addPeer(call, peerStream));
+}
+
+function hangUp(peerId) {
+  if (peers[peerId]) peers[peerId].close();
+}
+
+function addPeer(call, stream) {
+  peers[call.peer] = call;
+  var peerElem = document.createElement("div");
+  var audioElem = document.createElement("audio");
+  audioElem.srcObject = stream;
+  audioElem.addEventListener("loadedmetadata", () => audioElem.play());
+  peerElem.className = "peer";
+  peerElem.appendChild(document.createTextNode("User " + call.peer));
+  peerElem.appendChild(audioElem);
+  peerGrid.appendChild(peerElem);
+  call.on("close", () => peerElem.remove());
+}
