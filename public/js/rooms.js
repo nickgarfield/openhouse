5e5579d3bbbfbe452177f0c4dfd8711585b888d7
nickgarfield <ntgarfield@gmail.com>
2020-08-24 11:12:55 -0500

    👍 IMPROVE: Remove startRoom view

--- public/js/rooms.js
+++ public/js/rooms.js
@@ -1,5 +1,15 @@
 function startRoom() {
-  window.location.href = "/start";
+  fetch(window.location.protocol + "rooms", {
+    method: "POST",
+    cache: "no-cache",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      title: null
+    })
+  })
+    .then(res => res.json())
+    .then(room => (window.location.href = "/r/" + room.id))
+    .catch(e => console.log(e));
 }
 
 function joinRoom(e) {
