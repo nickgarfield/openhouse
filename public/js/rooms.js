28290726e8d4955a2222bf0c29a8e421cdd0a6f2
nickgarfield <ntgarfield@gmail.com>
2020-09-03 21:27:07 -0500

    📦 NEW: Hide start room feature

--- public/js/rooms.js
+++ public/js/rooms.js
@@ -1,17 +1,3 @@
-function startRoom() {
-  fetch(window.location.protocol + "rooms", {
-    method: "POST",
-    cache: "no-cache",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      title: null
-    })
-  })
-    .then(res => res.json())
-    .then(room => (window.location.href = "/r/" + room.id))
-    .catch(e => console.log(e));
-}
-
 function joinRoom(e) {
   e.preventDefault();
   window.location.href = "/r/" + e.target.name;
